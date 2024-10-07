8539e981d5fa59b9140f16062c192c0605c941e8
Lux <iansseijelly@berkeley.edu>
2024-10-06 20:21:11 -0700

    FIX: L-trace encoder fully compiles

--- riscv/processor.h
+++ riscv/processor.h
@@ -17,7 +17,7 @@
 #include "triggers.h"
 #include "../fesvr/memif.h"
 #include "vector_unit.h"
-#include "trace_encoder_n.h"
+#include "trace_encoder_l.h"
 
 #define FIRST_HPMCOUNTER 3
 #define N_HPMCOUNTERS 29
@@ -453,7 +453,7 @@ public:
 
   vectorUnit_t VU;
   triggers::module_t TM;
-  trace_encoder_n trace_encoder;
+  trace_encoder_l trace_encoder;
 };
 
 #endif
--- riscv/trace_encoder_l.cc
+++ riscv/trace_encoder_l.cc
@@ -22,11 +22,11 @@ void trace_encoder_l::push_ingress(hart_to_encoder_ingress_t packet) {
       this->state = TRACE_ENCODER_L_DATA;
     } else if (this->state == TRACE_ENCODER_L_DATA) {
       if (this->ingress_1.i_type == I_BRANCH_TAKEN) {
-        _generate_branch_taken_packet();
+        _generate_direct_packet(F_TB);
       } else if (this->ingress_1.i_type == I_BRANCH_NON_TAKEN) {
-        _generate_branch_non_taken_packet();
+        _generate_direct_packet(F_NT);
       } else if (this->ingress_1.i_type == I_JUMP_INFERABLE) {
-        _generate_jump_inferable_packet();
+        _generate_direct_packet(F_IJ);
       } else if (this->ingress_1.i_type == I_JUMP_UNINFERABLE) {
         _generate_jump_uninferable_packet();
       }
@@ -38,13 +38,17 @@ void trace_encoder_l::_generate_sync_packet() {
   // set packet fields
   this->packet.c_header = C_NA;
   this->packet.f_header = F_SYNC;
+  // set packet fields
   this->packet.address = this->ingress_0.i_addr;
+  this->prev_address = this->ingress_0.i_addr;
+  // set packet fields
   this->packet.timestamp = this->ingress_0.i_timestamp;
+  this->prev_timestamp = this->ingress_0.i_timestamp;
   // encode the packet
   int num_bytes = 0;
-  num_bytes += encode_non_compressed_header(&this->packet, this->buffer);
-  num_bytes += encode_varlen(this->packet.address, this->buffer + num_bytes);
-  num_bytes += encode_varlen(this->packet.timestamp, this->buffer + num_bytes);
+  num_bytes += _encode_non_compressed_header(&this->packet, this->buffer);
+  num_bytes += _encode_varlen(this->packet.address, this->buffer + num_bytes);
+  num_bytes += _encode_varlen(this->packet.timestamp, this->buffer + num_bytes);
   // write the packet to the trace sink
   fwrite(this->buffer, 1, num_bytes, this->trace_sink);
 }
@@ -58,11 +62,11 @@ void trace_encoder_l::_generate_direct_packet(f_header_t f_header) {
   this->packet.c_header = is_compressed ? get_c_header(f_header) : C_NA;
   this->packet.f_header = f_header;
   int num_bytes = 0;
-  if (is_compressed) { 
-    num_bytes += encode_compressed_packet(&this->packet, this->buffer); 
+  if (likely(is_compressed)) { 
+    num_bytes += _encode_compressed_packet(&this->packet, this->buffer); 
   } else {
-    num_bytes += encode_non_compressed_header(&this->packet, this->buffer);
-    num_bytes += encode_varlen(this->packet.timestamp, this->buffer + num_bytes);
+    num_bytes += _encode_non_compressed_header(&this->packet, this->buffer);
+    num_bytes += _encode_varlen(this->packet.timestamp, this->buffer + num_bytes);
   }
   fwrite(this->buffer, 1, num_bytes, this->trace_sink);
 }
@@ -72,17 +76,39 @@ void trace_encoder_l::_generate_jump_uninferable_packet() {
   this->packet.f_header = F_UJ;
   // calculate the address
   uint64_t e_addr = this->ingress_0.i_addr >> 1;
-  this->packet.address = e_addr ^ this->prev_addr;
-  this->prev_addr = e_addr;
+  this->packet.address = e_addr ^ this->prev_address;
+  this->prev_address = e_addr;
   // calculate the timestamp
   this->packet.timestamp = this->ingress_1.i_timestamp - this->prev_timestamp;
   this->prev_timestamp = this->ingress_1.i_timestamp;
   // encode the packet
   int num_bytes = 0;
-  num_bytes += encode_non_compressed_header(&this->packet, this->buffer);
-  num_bytes += encode_varlen(this->packet.address, this->buffer + num_bytes);
-  num_bytes += encode_varlen(this->packet.timestamp, this->buffer + num_bytes);
+  num_bytes += _encode_non_compressed_header(&this->packet, this->buffer);
+  num_bytes += _encode_varlen(this->packet.address, this->buffer + num_bytes);
+  num_bytes += _encode_varlen(this->packet.timestamp, this->buffer + num_bytes);
   fwrite(this->buffer, 1, num_bytes, this->trace_sink);
+}
+
+int trace_encoder_l::_encode_compressed_packet(trace_encoder_l_packet_t* packet, uint8_t* buffer) {
+  buffer[0] = packet->c_header | packet->timestamp << 2;
+  return 1;
+}
+
+int trace_encoder_l::_encode_non_compressed_header(trace_encoder_l_packet_t* packet, uint8_t* buffer) {
+  buffer[0] = packet->c_header | packet->f_header << 2;
+  return 1;
+}
+
+// encodes a uint64_t into a buffer using a variable-length encoding
+// returns the number of bytes used for encoding the address
+int trace_encoder_l::_encode_varlen(uint64_t value, uint8_t* buffer) {
+  int msb = find_msb(value);
+  int num_bytes = ceil_div(msb, 7);
+  for (int i = 0; i < num_bytes; i++) {
+    buffer[i] = (value & 0x7F) | (i == num_bytes - 1 ? 0x80 : 0x00);
+    value >>= 7;
+  }
+  return num_bytes;
 }
 
 // returns the 0-index of the most significant bit
@@ -96,31 +122,9 @@ int ceil_div(int a, int b) {
   return a / b + 1;
 }
 
-int encode_compressed_packet(trace_encoder_l_packet_t* packet, uint8_t* buffer) {
-  buffer[0] = packet->c_header | packet->timestamp << 2;
-  return 1;
-}
-
-int encode_non_compressed_header(trace_encoder_l_packet_t* packet, uint8_t* buffer) {
-  buffer[0] = packet->c_header | packet->f_header << 2;
-  return 1;
-}
-
-// encodes a uint64_t into a buffer using a variable-length encoding
-// returns the number of bytes used for encoding the address
-int encode_varlen(uint64_t value, uint8_t* buffer) {
-  int msb = find_msb(value);
-  int num_bytes = ceil_div(msb, 7);
-  for (int i = 0; i < num_bytes; i++) {
-    buffer[i] = (value & 0x7F) | (i == num_bytes - 1 ? 0x80 : 0x00);
-    value >>= 7;
-  }
-  return num_bytes;
-}
-
 c_header_t get_c_header(f_header_t f_header) {
-  if (f_header == F_BRANCH_TAKEN) return C_TB;
-  else if (f_header == F_BRANCH_NON_TAKEN) return C_NT;
-  else if (f_header == F_JUMP_INFERABLE) return C_IJ;
+  if (f_header == F_TB) return C_TB;
+  else if (f_header == F_NT) return C_NT;
+  else if (f_header == F_IJ) return C_IJ;
   else return C_NA;
 }
