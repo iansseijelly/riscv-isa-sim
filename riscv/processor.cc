e0b18b051b87d3171264b6766f234df61ecbc553
Jim Fang <yf328@berkeley.edu>
2025-02-01 17:33:34 -0800

    FIX: show encoder timepackets + erase prev trace logs only when run with trace flag

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -152,6 +152,9 @@ void processor_t::enable_log_commits()
 void processor_t::enable_trace()
 {
   trace_enabled = true;
+
+  // if trace flag is true, touch the file, otherwise leave it -- don't do in encoder ∵ encoder enable/disable toggles mid process
+  trace_encoder.init_trace_file();
 }
 
 void processor_t::reset()
